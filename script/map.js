2e2a441d31b058476610ad86f51347a53c27cc9f
mortargoblin <aaro.euro@gmail.com>
2025-04-25 14:18:21 +0300

    visual improvements

--- script/map.js
+++ script/map.js
@@ -1,16 +1,20 @@
-//import defaultExport from './leaflet.js';
+// Leflet map loader
+
 
 // Map initialization
-  const map = L.map('map', {attributionControl:false}).setView([50.073658, 14.418540], 4);
+const map = L.map('map', {
+  attributionControl: false,
+  zoomControl: false
+}).setView([50.073658, 14.418540], 4);
 
-  // stadia background layer
-  const stadia_bg = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
+// stadia background layer
+const stadia_bg = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 0,
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	ext: 'png'
 });
-  stadia_bg.addTo(map)
+stadia_bg.addTo(map)
 
 /*
 L.marker([50.073658, 14.418540]).addTo(map)
